1f9797efe0615635eb375a87385e6f6a2fc98c76
m-rashid <mustafa.rashidZA@gmail.com>
2017-05-02 20:11:28 +0200

    Define constructors and destructor for HuffmanTree class

--- HuffmanTree.cpp
+++ HuffmanTree.cpp
@@ -2,3 +2,26 @@
 // Created by Mustafa on 02/05/2017.
 //
 
+#include <memory>
+#include "HuffmanTree.h"
+using namespace std;
+
+namespace RSHMUS001 {
+
+    HuffmanTree::HuffmanTree() : root(nullptr) {}; //default constructor
+
+    HuffmanTree::HuffmanTree(const HuffmanTree & rhs) : root(rhs.root) {    //copy constructor
+
+    };
+
+    HuffmanTree::~HuffmanTree() {   //destructor
+        root = nullptr;
+    }
+
+    HuffmanTree::HuffmanTree(HuffmanTree && rhs) : root(std::move(rhs.root)){ //move constructor
+            rhs.root = nullptr;
+    }
+
+    
+}
+
