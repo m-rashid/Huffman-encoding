5292f05f8b2a349271396fe05682b097f0a22bc5
m-rashid <mustafa.rashidZA@gmail.com>
2017-05-02 18:23:29 +0200

    Define constructors and mutator/accessor methods for HuffmanNode

--- HuffmanNode.cpp
+++ HuffmanNode.cpp
@@ -2,3 +2,13 @@
 // Created by Mustafa on 02/05/2017.
 //
 
+#include <memory>
+#include "HuffmanNode.h
+using namespace std;
+
+namespace RSHMUS001{
+
+    HuffmanNode::HuffmanNode(const HuffmanNode & node) :
+
+}
+
