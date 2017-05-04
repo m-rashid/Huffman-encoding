543599ee967d87fa863b5909e4b3c50db96996eb
m-rashid <mustafa.rashidZA@gmail.com>
2017-05-04 09:04:53 +0200

    Implement compare method to compare two nodes

--- HuffmanNode.cpp
+++ HuffmanNode.cpp
@@ -8,7 +8,44 @@ using namespace std;
 
 namespace RSHMUS001{
 
-    HuffmanNode::HuffmanNode(const HuffmanNode & node) :
+    //default constructor
+    HuffmanNode::HuffmanNode() : frequency(0), letter(0){
 
+    }
+
+    //parametrized constructor
+    HuffmanNode::HuffmanNode(char l = '\0', int f = 0) : frequency(f), letter(l) {
+
+    }
+
+    HuffmanNode::~HuffmanNode(){
+
+    }
+
+    //copy constructor
+    HuffmanNode::HuffmanNode(const HuffmanNode & rhs) : letter(rhs.letter), frequency(rhs.frequency){
+
+    }
+
+    //move constructor
+    HuffmanNode::HuffmanNode (HuffmanNode && rhs) : letter(rhs.letter), frequency(rhs.frequency){
+        rhs.frequency = 0;
+    }
+
+    char HuffmanNode::getLetter(){
+        return letter;
+    }
+
+    int HuffmanNode::getFrequency(){
+        return frequency;
+    }
+
+    void HuffmanNode::setLetter(char l){
+        letter = l;
+    }
+
+    void HuffmanNode::setFreq(int f) {
+        frequency = f;
+    }
 }
 
--- HuffmanNode.h
+++ HuffmanNode.h
@@ -2,7 +2,36 @@
 // Created by Mustafa on 02/05/2017.
 //
 
-#ifndef ASSIGNMENT_3_HUFFMANNODE_H
-#define ASSIGNMENT_3_HUFFMANNODE_H
+#ifndef _HUFFMANNODE_H
+#define _HUFFMANNODE_H
 
-#endif //ASSIGNMENT_3_HUFFMANNODE_H
+#include <memory>
+
+namespace RSHMUS001 {
+
+    class HuffmanNode {
+
+    private:
+        char letter;
+        int frequency;
+        std::shared_ptr<HuffmanNode> left;
+        std::shared_ptr<HuffmanNode> right;
+
+    public:
+        HuffmanNode();                          //default constructor
+        HuffmanNode(char letter, int freq);     //parameterized constructor
+        ~HuffmanNode();                         //destructor
+        HuffmanNode(const HuffmanNode& node);   //copy constructor
+        HuffmanNode(HuffmanNode&& node);        //move constructor
+
+        //accessors
+        char getLetter();
+        int getFrequency();
+
+        //mutators
+        void setLetter(char l);
+        void setFreq(int f);
+
+    };
+}
+#endif //_HUFFMANNODE_H
