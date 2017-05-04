6e92a1838d7caa510b62f51dc098edeab2c2544f
m-rashid <mustafa.rashidZA@gmail.com>
2017-05-04 12:53:55 +0200

    Define buildTree() function in class HuffmanTree

--- HuffmanTree.cpp
+++ HuffmanTree.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <memory>
+#include <queue>
 #include "HuffmanTree.h"
 using namespace std;
 
@@ -19,9 +20,57 @@ namespace RSHMUS001 {
     }
 
     HuffmanTree::HuffmanTree(HuffmanTree && rhs) : root(std::move(rhs.root)){ //move constructor
-            rhs.root = nullptr;
+        rhs.root = nullptr;
     }
 
-    
+    /*
+    bool compare (const HuffmanNode& a, const HuffmanNode& b){
+        if(a<b){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+     */
+
+    struct compare
+    {
+        bool operator()(const HuffmanNode& a, const HuffmanNode& b)
+        {
+            return a.getFrequency() < b.getFrequency();
+        }
+    };
+
+
+    void HuffmanTree::buildTree(unordered_map<char, int> & frequencyTable){
+
+        priority_queue<HuffmanNode,vector<HuffmanNode>, compare > pq;
+
+        for(auto it = frequencyTable.begin(); it != frequencyTable.end(); ++it){
+            char l = it -> first;
+            int f = it -> second;
+            HuffmanNode node (l, f);
+            pq.push(node);
+        }
+
+        while (pq.size() > 1){
+
+            HuffmanNode parent;
+            HuffmanNode l = pq.top(); //left child
+            pq.pop();
+            HuffmanNode r = pq.top(); //right child
+            pq.top();
+
+            parent.setLeft(l);
+            parent.setRight(r);
+            parent.setFreq(l.getFrequency() + r.getFrequency());
+            parent.setLetter('\0');
+
+            pq.push(parent);
+        }
+
+
+    }
 }
 
